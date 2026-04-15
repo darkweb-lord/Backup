f5b1c44c100f3d7146304f00d1bcba8d6133d9be
SG <darkwebking07@gmail.com>
2026-04-15 18:40:07 +0530

    15-04-2026 Daily Backup

---
+++ src_15042026/I2c_Header.h
@@ -0,0 +1,14 @@
+#ifndef I2C_HEADER_H
+#define I2C_HEADER_H
+
+#include <xc.h> // Added to ensure uint8_t and SFRs are known to headers
+#include <stdint.h>
+
+void I2C_INIT(void);
+void I2C1_WAIT(void);
+void I2C1_START(void);
+void I2C1_STOP(void);
+uint8_t I2C1_WRITE(uint8_t data);
+
+#endif /* I2C_HEADER_H */ 
+// Removed the extra #endif that was on line 21
